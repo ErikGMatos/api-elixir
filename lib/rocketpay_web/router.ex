3d5904f946c3ff952c06f1f47048f545d2b3a2e5
Erik Matos <erik_senac@hotmail.com>
2021-02-27 13:28:16 -0300

    feat(cors): Cors

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -11,11 +11,6 @@ defmodule RocketpayWeb.Router do
     plug :basic_auth, Application.compile_env(:rocketpay, :basic_auth)
   end
 
-  pipeline :api do
-    plug CORSPlug, origin: "*"
-    plug :accepts, ["json"]
-  end
-
   scope "/api", RocketpayWeb do
     pipe_through :api
 
